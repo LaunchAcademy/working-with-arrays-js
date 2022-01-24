85d9a0056b28fbd6d4f434f1c5d9ca92557fe7dd
Stephen Fang <safang99@gmail.com>
2022-01-24 11:47:50 -0500

    starter code

---
+++ main.js
@@ -0,0 +1,55 @@
+// Meal Planning
+let groceries = ["apples", "oranges", "cookies", "salad"]
+// groceries = []
+// groceries.push("apples")
+// groceries.push("oranges")
+// groceries.push("cookies")
+
+// What was the first item I added to my list?
+// Output it.
+
+// What was the last item I added to my list?
+// Output it.
+
+// Add cucumbers to the end of list
+// What is the index of the cucumbers entry?
+
+// I found another list in the utensil drawer.
+// Combine it with what we have
+const otherList = ["tomatoes", "crackers", "soup", "chips"]
+
+
+// We already have tomatoes.
+// If they're on the list, remove them
+
+// Provide a friendly output of the list
+// Add a bullet before each item
+// groceries.forEach(grocery => {
+//   console.log(`* ${grocery}`)
+// })
+
+// Sort the list alphabetically
+// Output the list
+
+// Add items to your shopping list
+// Loop until done
+// let input
+// do {
+//   input = prompt("What would you like to add?")
+//   groceries.push(input)
+// } while (input !== "done")
+// groceries.pop()
+
+// console.log(groceries)
+
+// let input
+// do {
+//   input = prompt("What would you like to add?")
+//   if (input !== "done") {
+//     groceries.push(input)
+//   }
+// } while (input !== "done")
+
+// I only have time to shop for items starting
+// with the letter C
+// Build a new array based off my list
